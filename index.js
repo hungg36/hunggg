dd9d80ddcc9ed7fbfd7917dee60b05927bfa9a5a
hungg36 <hungnguyen1372008@gmail.com>
2025-11-02 19:46:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import http from "http";
+
 const port = process.env.PORT || 3000;
-http.createServer((req, res) => {
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello from Render VPS!");
-}).listen(port, () => console.log("Server started on port", port));
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("Server started on port", port);
+});
